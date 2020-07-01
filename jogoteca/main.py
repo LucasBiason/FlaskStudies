9b7b282b8396c175f617dda4b475b3d5628e1066
Lucas Biason <biasonlucky@hotmail.com>
2020-07-01 18:40:53 -0300

    jogoteca - Aula 03 - Cadastro de Jogo

--- jogoteca/main.py
+++ jogoteca/main.py
@@ -1,17 +1,43 @@
+import flask
 from flask  import Flask, render_template
 from models import Jogo
 
 app = Flask(__name__)
+app.secret_key = 'SECRET_KEY'
 
-@app.route('/')
-def teste():
+
+JOGOS =  Jogo.get_jogos()
+
+@app.route('/home')
+def listagem():
     context = {
         'titulo': 'Jogos',
-        'lista_jogos': Jogo.get_jogos()
+        'lista_jogos': JOGOS
     }
     return render_template(
         'lista.html', ** context
     )
+    
+
+@app.route('/novo', methods=['GET', 'POST' ])
+def cadastro():
+    context = {
+        'titulo': 'Jogos - Cadastro',
+    }
+    
+    if flask.request.method == 'POST':
+        nome =  flask.request.form.get('nome')
+        categoria =  flask.request.form.get('categoria')
+        console =  flask.request.form.get('console')
+        jogo_novo = Jogo(nome, categoria, console)
+        JOGOS.append(jogo_novo)
+        flask.flash(f'Jogo {jogo_novo.nome} cadastrado com sucesso.', 'success')
+        return flask.redirect('/home')
+        
+    return render_template(
+        'novo.html', ** context
+    )
+
 
 # run the application
 if __name__ == "__main__":
