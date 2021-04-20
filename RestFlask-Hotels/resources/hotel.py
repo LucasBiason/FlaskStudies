59b73abbf602f7f4eaa649b1154c2424fe1902a9
Lucas Biason <biasonlucky@hotmail.com>
2021-04-20 17:04:39 -0300

    [RestFlask-Hotels] CRUD Memory

--- RestFlask-Hotels/resources/hotel.py
+++ RestFlask-Hotels/resources/hotel.py
@@ -1,23 +1,23 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
         'hotel_id': 'alpha',
-        'name': 'Alpha Hotel',
+        'nome': 'Alpha Hotel',
         'estrelas': 4.3,
         'diaria': 420.34,
         'cidade': 'Rio de Janeiro'
     },
     {
         'hotel_id': 'beta',
-        'name': 'Beta Hotel',
+        'nome': 'Beta Hotel',
         'estrelas': 4.7,
         'diaria': 390.99,
         'cidade': 'São Paulo'
     },
     {
         'hotel_id': 'gama',
-        'name': 'Gama Hotel',
+        'nome': 'Gama Hotel',
         'estrelas': 4.5,
         'diaria': 320.30,
         'cidade': 'Santa Catarina'
@@ -25,5 +25,49 @@ hoteis = [
 ]
 
 class Hoteis(Resource):
+
     def get(self):
         return {'hoteis': hoteis}
+
+class Hotel(Resource):
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome')
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
+
+    def find_hotel(hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return False
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
+        return {'message': 'Hotel not found.'}, 404
+
+    def post(self, hotel_id):
+        dados = Hotel.atributos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
+
+    def put(self, hotel_id):
+        dados = Hotel.atributos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return hotel, 200
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
+
+    def delete(self, hotel_id):
+        global hoteis
+        hotel = Hotel.find_hotel(hotel_id)
+        if not hotel:
+            return {'message': 'Hotel not found.'}, 404
+        hoteis.remove(hotel)
+        return {'message': 'Hotel deleted.'}, 200
